73fbd5da1a5f9ac9f3f46cdd38052bbd43de49d2
EduardoMascarenhas <eduardomsmascarenhas@gmail.com>
2024-10-14 15:27:37 -0300

    ajustes para build

--- src/views/ui-elements/advance/UITreeview/MultiSelectTreeView.tsx
+++ src/views/ui-elements/advance/UITreeview/MultiSelectTreeView.tsx
@@ -1,27 +1,21 @@
-import { TreeItem, TreeView } from '@mui/x-tree-view';
-
-// assets
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-
-// ==============================|| UI TREEVIEW - MULTI-SELECT ||============================== //
+import { TreeItem, SimpleTreeView } from '@mui/x-tree-view';
 
 export default function MultiSelectTreeView() {
     return (
-        <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />} multiSelect>
-            <TreeItem nodeId="1" label="Applications">
-                <TreeItem nodeId="2" label="Calendar" />
-                <TreeItem nodeId="3" label="Chrome" />
-                <TreeItem nodeId="4" label="Webstorm" />
+        <SimpleTreeView multiSelect>
+            <TreeItem itemId="1" label="Applications">
+                <TreeItem itemId="2" label="Calendar" />
+                <TreeItem itemId="3" label="Chrome" />
+                <TreeItem itemId="4" label="Webstorm" />
             </TreeItem>
-            <TreeItem nodeId="5" label="Documents">
-                <TreeItem nodeId="6" label="Material-UI">
-                    <TreeItem nodeId="7" label="src">
-                        <TreeItem nodeId="8" label="index.js" />
-                        <TreeItem nodeId="9" label="tree-view.js" />
+            <TreeItem itemId="5" label="Documents">
+                <TreeItem itemId="6" label="Material-UI">
+                    <TreeItem itemId="7" label="src">
+                        <TreeItem itemId="8" label="index.js" />
+                        <TreeItem itemId="9" label="tree-view.js" />
                     </TreeItem>
                 </TreeItem>
             </TreeItem>
-        </TreeView>
+        </SimpleTreeView>
     );
 }
--- src/views/ui-elements/advance/UITreeview/RecursiveTreeView.tsx
+++ src/views/ui-elements/advance/UITreeview/RecursiveTreeView.tsx
@@ -1,8 +1,4 @@
-import { TreeItem, TreeView } from '@mui/x-tree-view';
-
-// assets
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { TreeItem, SimpleTreeView } from '@mui/x-tree-view';
 
 interface RTVChildrenProps {
     id: string;
@@ -35,14 +31,10 @@ const data = {
 
 export default function RecursiveTreeView() {
     const renderTree = (nodes: RTVChildrenProps) => (
-        <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+        <TreeItem key={nodes.id} itemId={nodes.id} label={nodes.name}>
             {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
         </TreeItem>
     );
 
-    return (
-        <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpanded={['root']} defaultExpandIcon={<ChevronRightIcon />}>
-            {renderTree(data)}
-        </TreeView>
-    );
+    return <SimpleTreeView defaultExpandedItems={['root']}>{renderTree(data)}</SimpleTreeView>;
 }
