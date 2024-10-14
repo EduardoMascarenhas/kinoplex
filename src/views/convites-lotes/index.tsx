6f7a1d912c01c2d7661c05ce19e04385b646d895
EduardoMascarenhas <eduardomsmascarenhas@gmail.com>
2024-10-14 16:23:51 -0300

    ajuste do build vercel

---
+++ src/views/convites-lotes/index.tsx
@@ -0,0 +1,12 @@
+import ConvitesLotes from 'ui-component/cards/ConvitesLotes';
+import MainCardConvitesLotes from 'ui-component/cards/MainCardConvitesLotes';
+
+function ConviteLoteList() {
+    return (
+        <MainCardConvitesLotes title="Convites - Lotes">
+            <ConvitesLotes />
+        </MainCardConvitesLotes>
+    );
+}
+
+export default ConviteLoteList;
